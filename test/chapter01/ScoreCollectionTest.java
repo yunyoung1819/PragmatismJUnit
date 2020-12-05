34e9fbcfae562a5fd1fe6eefe1a654365453152b
yunyoung1819 <yunyoung1819@hanmail.net>
2020-12-05 21:38:39 +0900

    # ScoreCollectionTest 코드 작

--- test/chapter01/ScoreCollectionTest.java
+++ test/chapter01/ScoreCollectionTest.java
@@ -1,14 +1,23 @@
 package chapter01;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class ScoreCollectionTest {
-
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void answersArithmeticMeanOfTwoNumbers() {
+		// 준비
+		ScoreCollection collection = new ScoreCollection();
+		collection.add(() -> 5);
+		collection.add(() -> 7);
+		
+		// 실행
+		int actualResult = collection.arithmeticMean();
+		
+		// 단언
+		assertThat(actualResult, equalTo(6));
 	}
-
 }
